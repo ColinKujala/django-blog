8a7ec047154b3103ab96bb78a41550ee9d51c6fc
ColinKujala <111202947+ColinKujala@users.noreply.github.com>
2025-01-07 09:48:22 -0800

    working on polling app. Made Poll model

--- polling/models.py
+++ polling/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.CharField(blank=True, max_length=512)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
