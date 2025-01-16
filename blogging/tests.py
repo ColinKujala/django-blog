f5ee21ce81417c9bf1c383f17dd32a08e39c223b
ColinKujala <111202947+ColinKujala@users.noreply.github.com>
2025-01-16 12:32:07 -0800

    applied black to the repository files

--- blogging/tests.py
+++ blogging/tests.py
@@ -3,6 +3,7 @@ import datetime
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.utils.timezone import now
+
 """
 Course content had me import utc from django.utils.timezone, but it didn't
 work. I assume because it is deprecated from the older version. I instead
@@ -16,14 +17,15 @@ from blogging.models import Post, Category
 
 class FrontEndTestCase(TestCase):
     """test views privided in the front-end"""
+
     fixtures = [
-        'blogging_test_fixture.json',
+        "blogging_test_fixture.json",
     ]
 
     def setUp(self):
-        self.now = datetime.datetime.now(
-            datetime.timezone.utc
-        ).replace(tzinfo=now().tzinfo)
+        self.now = datetime.datetime.now(datetime.timezone.utc).replace(
+            tzinfo=now().tzinfo
+        )
         """
         above line instead of doing datetime.datetime.utcnow(), which was
         deprecated, I did the now function and passed into that function
@@ -32,19 +34,15 @@ class FrontEndTestCase(TestCase):
         self.timedelta = datetime.timedelta(15)
         author = User.objects.get(pk=1)
         for count in range(1, 11):
-            post = Post(
-                title="Post %d Title" % count,
-                text="foo",
-                author=author
-            )
+            post = Post(title="Post %d Title" % count, text="foo", author=author)
             if count < 6:
                 # publish the first five posts
                 pubdate = self.now - self.timedelta * count
                 post.published_date = pubdate
             post.save()
-    
+
     def test_list_only_published(self):
-        resp = self.client.get('/')
+        resp = self.client.get("/")
         # The content of the renered response is always a byte-string
         resp_text = resp.content.decode(resp.charset)
         self.assertTrue("My Cool Blog Posts" in resp_text)
@@ -54,12 +52,12 @@ class FrontEndTestCase(TestCase):
                 self.assertContains(resp, title, count=1)
             else:
                 self.assertNotContains(resp, title)
-    
+
     def test_details_only_published(self):
         for count in range(1, 11):
             title = "Post %d Title" % count
             post = Post.objects.get(title=title)
-            resp = self.client.get('/posts/%d' % post.pk, follow=True)
+            resp = self.client.get("/posts/%d" % post.pk, follow=True)
             """
             Note to self
 
@@ -77,11 +75,13 @@ class FrontEndTestCase(TestCase):
 
 
 class PostTestCase(TestCase):
-    fixtures = ['blogging_test_fixture.json', ]
+    fixtures = [
+        "blogging_test_fixture.json",
+    ]
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
-    
+
     def test_string_representation(self):
         expected = "This is a title"
         p1 = Post(title=expected)
